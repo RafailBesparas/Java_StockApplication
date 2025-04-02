1ca504707212c1cb46ee975dc510d06d5d9a5d59
RafaelBesparas <58226725+RafaelBesparas@users.noreply.github.com>
2025-04-02 13:50:58 +0200

    Adding dependencies and also the base

---
+++ tradersdesk/src/main/java/com/besparas/TradersdeskApplication.java
@@ -0,0 +1,13 @@
+package com.besparas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradersdeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradersdeskApplication.class, args);
+	}
+
+}
---
+++ tradersdesk/src/test/java/com/besparas/TradersdeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.besparas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradersdeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
