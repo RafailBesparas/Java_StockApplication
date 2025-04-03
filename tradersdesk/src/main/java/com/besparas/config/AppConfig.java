a07faeb6ef6533c19c9c949b55977ffe3c779df9
RafaelBesparas <58226725+RafaelBesparas@users.noreply.github.com>
2025-04-03 13:45:04 +0200

    Create the authentication methods and back end

--- tradersdesk/src/main/java/com/besparas/config/AppConfig.java
+++ tradersdesk/src/main/java/com/besparas/config/AppConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 public class AppConfig {
@@ -11,8 +13,17 @@ public class AppConfig {
     // Here we will provide the protected end points and the white listed
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http.sessionManagement(management->management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests();
+                .authorizeHttpRequests(Authorize -> Authorize.requestMatchers("/api/**").authenticated()
+                        .anyRequest().permitAll())
+                .addFilterBefore(new JwtTokenValidator(), BasicAuthenticationFilter.class)
+                .csrf(csrf -> csrf.disable())
+                .cors(cors-> cors.configurationSource(corsConfigurationSource()));
 
+
+        return http.build();
+    }
+
+    private CorsConfigurationSource corsConfigurationSource() {
         return null;
     }
 }
---
+++ tradersdesk/src/main/java/com/besparas/config/JwtConstant.java
@@ -0,0 +1,8 @@
+package com.besparas.config;
+
+public class JwtConstant {
+    public static final String SECRET_Key = "RandomString";
+
+    public static final String JWT_HEADER = "Authorization";
+
+}
